2b69a19fc89c2007cae123133a0d113de84be959
rustamnova <novamedia777@gmail.com>
2023-02-07 13:25:21 +0300

    v.01

--- pong.py
+++ pong.py
@@ -11,29 +11,50 @@ def update_score(l_score, r_score, player, score_board):
         font=('Arial', 24, "normal"))
     return l_score, r_score, score_board
 
+def setup_game():
+    screen = turtle.Screen()
+    screen.title("Pong Game")
+    screen.bgcolor("black")
+    screen.setup(width=600, height=400)
+    screen.tracer(0)
+
+    ball = turtle.Turtle()
+    ball.shape("square")
+    ball.color("white")
+    ball.penup()
+    ball.speed(0)
+    ball.goto(0, 0)
+    ball.dx = 2
+    ball.dy = 2
+
+    l_paddle = turtle.Turtle()
+    l_paddle.shape("square")
+    l_paddle.color("white")
+    l_paddle.penup()
+    l_paddle.speed(0)
+    l_paddle.goto(-250, 0)
+    l_paddle.shapesize(stretch_wid=5, stretch_len=1)
+
+    r_paddle = turtle.Turtle()
+    r_paddle.shape("square")
+    r_paddle.color("white")
+    r_paddle.penup()
+    r_paddle.speed(0)
+    r_paddle.goto(250, 0)
+    r_paddle.shapesize(stretch_wid=5, stretch_len=1)
+
+    score_board = turtle.Turtle()
+    score_board.penup()
+    score_board.color("white")
+    score_board.hideturtle()
+    score_board.goto(0, 160)
+    score_board.write("Left Player: 0 -- Right Player: 0", align='center',
+                      font=('Arial', 24, "normal"))
+
+    return screen, ball, l_paddle, r_paddle, score_board
+
 def pong_game():
     game_components = setup_game()
     screen = game_components[0]
     ball = game_components[1]
-    l_paddle = game_components[2]
-    r_paddle = game_components[3]
-    l_score = 0
-    r_score = 0
-
-    def l_paddle_up():
-        l_paddle.sety(l_paddle.ycor() + 20)
-
-    def l_paddle_down():
-        l_paddle.sety(l_paddle.ycor() - 20)
-
-    def r_paddle_up():
-        r_paddle.sety(r_paddle.ycor() + 20)
-
-    def r_paddle_down():
-        r_paddle.sety(r_paddle.ycor() - 20)
-
-    screen.listen()
-    screen.onkeypress(l_paddle_up, 'w')
-    screen.onkeypress(l_paddle_down, 's')
-    screen.onkeypress(r_paddle_up, 'Up')
-    screen.onkeypress(r_paddle_down, 'Down')
+    l_paddle =
